24421bc63d699b65acd8f38457ac5bac5032b53e
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-07-11 13:37:02 +0200

    Update journalentrycontroller to support full CRUD

---
+++ BackEnd/Lumina.Api/Lumina.Api.ASP/helpers/UserHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+
+namespace Lumina.Api.ASP.helpers
+{
+    public static class UserHelper
+    {
+        public static bool IsUserLoggedIn(ClaimsPrincipal user)
+        {
+            return user?.FindFirst(ClaimTypes.NameIdentifier) != null;
+        }
+
+        public static bool TryGetUserId(ClaimsPrincipal user, out string userId)
+        {
+            userId = user?.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+            return !string.IsNullOrEmpty(userId);
+        }
+    }
+}
