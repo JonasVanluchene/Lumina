b8fe82f7961a61eea6193148ac1278a30f6a7de1
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-07-12 08:37:28 +0200

    Create Tag Dto's

---
+++ BackEnd/Lumina.Api/Lumina.DTO/Tag/TagDto.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Lumina.DTO.Tag
+{
+    public class TagDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BackEnd/Lumina.Api/Lumina.DTO/Tag/UserTagDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lumina.DTO.Tag
+{
+    public class UserTagDto
+    {
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
